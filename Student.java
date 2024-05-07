dc7c99e149a4df6ca4b06734059f517bdb875907
Pia Katleya V. Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2024-05-07 23:55:38 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,23 +1,18 @@
 public class Student {
   
     private String name;
+    private String studentNumber;
 
-    public Student(String name) {
+    public Student(String name, String studentNumber) {
         this.name = name;
+        this.studentNumber = studentNumber;
     }
 
     public void borrowResource(LibraryResource resource) {
-        System.out.println(name + " is borrowing: " + resource.getTitle());
+      
+        System.out.println("Student Name: " + name);
+        System.out.println("Student No: " + studentNumber);
+        System.out.println("Title: " + resource.getTitle());
+        System.out.println("Classification: " + resource.getClassification());
         resource.checkOutResource();
     }
-
-    // Getter for the student's name
-    public String getName() {
-        return name;
-    }
-
-    // Setter for the student's name
-    public void setName(String name) {
-        this.name = name;
-    }
-}
