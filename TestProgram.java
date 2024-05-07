76756ae2abf28824686052da3386edec2de3c842
Pia Katleya V. Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2024-05-07 23:58:48 +0800

    Update TestProgram.java

--- TestProgram.java
+++ TestProgram.java
@@ -2,12 +2,13 @@ public class TestProgram {
   
     public static void main(String[] args) {
       
-        Student student1 = new Student("Levi");
+        Student student = new Student("Himiko Toga", "123456789");
         LibraryResource book = new Book("OOP Book");
-        student1.checkOutResource(book);
+        student.borrowResource(book);
 
-        Student student2 = new Student("Mikasa");
+        Student student = new Student("Shoto Todoroki", "123455555");
         LibraryResource journal = new Journal("SOLID Principles Journal");
-        student2.checkOutResource(journal);
+        student.borrowResource(journal);
+
     }
 }
